bd8707018179fe0f292d0ab4bf2e8d8e2114e25a
Benjamin Schmid <bene.schmid@gmail.com>
2015-04-18 07:27:15 +0200

    Make the buttons nicer.

--- src/main/java/buttons/AWTButtons.java
+++ src/main/java/buttons/AWTButtons.java
@@ -1,7 +1,12 @@
 package buttons;
 
 import java.awt.Button;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.Panel;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -18,12 +23,40 @@ public class AWTButtons extends AbstractButtons {
 	private final Button laserButton = new Button("L");
 
 	public AWTButtons() {
-		panel.add(yDownButton);
-		panel.add(yUpButton);
-		panel.add(zDownButton);
-		panel.add(zUpButton);
-		panel.add(stackButton);
-		panel.add(laserButton);
+		GridBagLayout gridbag = new GridBagLayout();
+		GridBagConstraints c = new GridBagConstraints();
+		panel.setLayout(gridbag);
+
+		panel.setBackground(Color.BLACK);
+		panel.setForeground(Color.white);
+
+		c.anchor = GridBagConstraints.CENTER;
+		c.insets = new Insets(0, 0, 0, 0);
+
+		c.gridx = 1;
+		c.gridy = 0;
+		panel.add(yUpButton, c);
+
+		c.gridx = 1;
+		c.gridy = 1;
+		panel.add(yDownButton, c);
+
+		c.gridx = 0;
+		c.gridy = 1;
+		panel.add(zDownButton, c);
+
+		c.gridx = 2;
+		c.gridy = 1;
+		panel.add(zUpButton, c);
+
+		c.gridy = 0;
+		c.gridx = 3;
+		c.insets = new Insets(0, 7, 0, 7);
+		panel.add(stackButton, c);
+
+		c.gridy = 1;
+		c.gridx = 3;
+		panel.add(laserButton, c);
 
 		yDownButton.addMouseListener(new MouseAdapter(BUTTON_Y_DOWN));
 		yUpButton.addMouseListener(new MouseAdapter(BUTTON_Y_UP));
@@ -59,6 +92,13 @@ public class AWTButtons extends AbstractButtons {
 
 		public void mouseExited(MouseEvent e) {}
 
+	}
+
+	public static void main(String... args) {
+		Frame f = new Frame();
+		f.add(new AWTButtons().getPanel());
+		f.pack();
+		f.setVisible(true);
 	}
 
 }
