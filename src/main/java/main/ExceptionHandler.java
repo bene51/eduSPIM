d70aee08ad091831256abd1e67fede791c361b64
Benjamin Schmid <bene.schmid@gmail.com>
2015-04-28 06:47:54 +0200

    Implement a message box for showing exceptions.

--- src/main/java/main/ExceptionHandler.java
+++ src/main/java/main/ExceptionHandler.java
@@ -3,19 +3,31 @@ package main;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import javax.swing.JComponent;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
 public class ExceptionHandler {
 
 	public static void handleException(Throwable e) {
+		Mail.sendError(exceptionToString(e));
+		// TODO also log it
+	}
+
+	private static String exceptionToString(Throwable e) {
 		StringWriter sw = new StringWriter();
 		PrintWriter pw = new PrintWriter(sw);
 		e.printStackTrace(pw);
-		Mail.sendError(sw.toString());
-		// TODO also log it
+		return sw.toString();
 	}
 
 	public static void showException(Throwable e) {
 		e.printStackTrace();
-		// TODO add implementation
+		String s = exceptionToString(e);
+		JTextArea ta = new JTextArea(s);
+		final JComponent[] inputs = new JComponent[] { new JScrollPane(ta) };
+		JOptionPane.showMessageDialog(null, inputs, "Exception", JOptionPane.ERROR_MESSAGE);
 	}
 
 	public static void main(String[] args) {
@@ -23,7 +35,8 @@ public class ExceptionHandler {
 		try {
 			throw(e);
 		} catch(Exception ex) {
-			handleException(ex);
+			// handleException(ex);
+			showException(ex);
 		}
 	}
 }
