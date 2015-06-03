0ef60a1e08225ba14bb15ec933892510cddd1c8c
Benjamin Schmid <bene.schmid@gmail.com>
2015-06-03 18:22:09 +0200

    Introduce some delay after sending commands to the laser.

--- src/main/java/laser/Toptica.java
+++ src/main/java/laser/Toptica.java
@@ -26,6 +26,7 @@ public class Toptica implements ILaser {
 			port.writeString("en 1"        + TERM_CHAR);
 			port.writeString("en 2"        + TERM_CHAR);
 			port.writeString("en ext"      + TERM_CHAR);
+			sleep(20);
 		} catch(Exception e) {
 			throw new LaserException("Cannot initialize communication to laser ", e);
 		}
@@ -47,6 +48,7 @@ public class Toptica implements ILaser {
 	public void setPower(double power) throws LaserException {
 		try {
 			port.writeString("ch 2 pow " + (int)Math.round(power) + TERM_CHAR);
+			sleep(20);
 		} catch (Exception e) {
 			throw new LaserException("Error setting laser power", e);
 		}
@@ -57,6 +59,7 @@ public class Toptica implements ILaser {
 		try {
 			port.writeString("la on"   + TERM_CHAR);
 			port.writeString("dis ext" + TERM_CHAR);
+			sleep(20);
 		} catch (Exception e) {
 			throw new LaserException("Error switching laser to continuous mode", e);
 		}
@@ -67,6 +70,7 @@ public class Toptica implements ILaser {
 		try {
 			port.writeString("la on"   + TERM_CHAR);
 			port.writeString("en ext"  + TERM_CHAR);
+			sleep(20);
 		} catch (Exception e) {
 			throw new LaserException("Error switching laser to continuous mode", e);
 		}
@@ -76,6 +80,7 @@ public class Toptica implements ILaser {
 	public void setOff() throws LaserException {
 		try {
 			port.writeString("la off" + TERM_CHAR);
+			sleep(20);
 		} catch (Exception e) {
 			throw new LaserException("Error switching laser off", e);
 		}
@@ -84,5 +89,13 @@ public class Toptica implements ILaser {
 	@Override
 	public double getMaxPower() {
 		return MAX_POWER;
+	}
+
+	private static void sleep(long millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			ExceptionHandler.handleException("Interrupted during artificial delay", e);
+		}
 	}
 }
