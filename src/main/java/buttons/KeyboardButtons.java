303a7f3805b012368e1f7424aee3fd18aafeb8ea
Benjamin Schmid <bene.schmid@gmail.com>
2015-06-17 23:26:59 +0200

    Avoid a problem with auto-repeat of keyboard keys.

--- src/main/java/buttons/KeyboardButtons.java
+++ src/main/java/buttons/KeyboardButtons.java
@@ -11,10 +11,14 @@ public class KeyboardButtons extends AbstractButtons implements KeyListener {
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		if(super.getButtonDown() != -1)
-			return;
-
 		int kc = e.getKeyCode();
+
+		if(super.getButtonDown() != -1) {
+			if(kc == KeyEvent.VK_ESCAPE)
+				super.fireButtonReleased(super.getButtonDown());
+			return;
+		}
+
 		switch(kc) {
 		case KeyEvent.VK_UP:    super.fireButtonPressed(BUTTON_Y_UP);   break;
 		case KeyEvent.VK_DOWN:  super.fireButtonPressed(BUTTON_Y_DOWN); break;
@@ -28,23 +32,22 @@ public class KeyboardButtons extends AbstractButtons implements KeyListener {
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		int down = super.getButtonDown();
-		if(down == -1)
-			return;
-
-		int kc = e.getKeyCode();
-		switch(kc) {
-		case KeyEvent.VK_UP:    if(down == BUTTON_Y_UP)   super.fireButtonReleased(down); break;
-		case KeyEvent.VK_DOWN:  if(down == BUTTON_Y_DOWN) super.fireButtonReleased(down); break;
-		case KeyEvent.VK_RIGHT: if(down == BUTTON_Z_DOWN) super.fireButtonReleased(down); break;
-		case KeyEvent.VK_LEFT:  if(down == BUTTON_Z_UP)   super.fireButtonReleased(down); break;
-		case KeyEvent.VK_S:     if(down == BUTTON_STACK)  super.fireButtonReleased(down); break;
-		case KeyEvent.VK_L:     if(down == BUTTON_LASER)  super.fireButtonReleased(down); break;
-		case KeyEvent.VK_I:     if(down == BUTTON_INFO)   super.fireButtonReleased(down); break;
-		}
+//		int down = super.getButtonDown();
+//		if(down == -1)
+//			return;
+//
+//		int kc = e.getKeyCode();
+//		switch(kc) {
+//		case KeyEvent.VK_UP:    if(down == BUTTON_Y_UP)   super.fireButtonReleased(down); break;
+//		case KeyEvent.VK_DOWN:  if(down == BUTTON_Y_DOWN) super.fireButtonReleased(down); break;
+//		case KeyEvent.VK_RIGHT: if(down == BUTTON_Z_DOWN) super.fireButtonReleased(down); break;
+//		case KeyEvent.VK_LEFT:  if(down == BUTTON_Z_UP)   super.fireButtonReleased(down); break;
+//		case KeyEvent.VK_S:     if(down == BUTTON_STACK)  super.fireButtonReleased(down); break;
+//		case KeyEvent.VK_L:     if(down == BUTTON_LASER)  super.fireButtonReleased(down); break;
+//		case KeyEvent.VK_I:     if(down == BUTTON_INFO)   super.fireButtonReleased(down); break;
+//		}
 	}
 
 	@Override
 	public void close() throws ButtonsException {}
-
 }
