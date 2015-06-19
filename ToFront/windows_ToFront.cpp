6a9a0c8c94b899e45aba8c49e07080844f80b08c
Benjamin Schmid <bene.schmid@gmail.com>
2015-06-19 13:23:31 +0200

    Remove some debug messages.

--- ToFront/windows_ToFront.cpp
+++ ToFront/windows_ToFront.cpp
@@ -6,22 +6,20 @@ void toFront(void)
 	HWND hndl = NULL;
 	printf("hi\n");
 	hndl = FindWindowEx(NULL, NULL, NULL, "Sponsored session");
-	printf("Sponsored session window = %p\n", hndl);
 	if(hndl != NULL) {
 		BOOL tmp = SetForegroundWindow(hndl);
 		if(tmp == NULL) {
-			printf("SetForegroundWindow failed\n");
+			printf("Could not close teamviewer window\n", hndl);
 		} else {
 			keybd_event(VK_RETURN, 0, KEYEVENTF_EXTENDEDKEY | 0, 0);
 			keybd_event(VK_RETURN, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+			printf("Closed teamviewer window\n", hndl);
 		}
 	}
 
 	hndl = FindWindowEx(NULL, NULL, NULL, "Display");
-	printf("eduSPIM window = %p\n", hndl);
 	if(hndl != NULL) {
 		BOOL tmp = SetForegroundWindow(hndl);
-		printf("SetForegroundWindow failed\n");
 	}
 }
 
